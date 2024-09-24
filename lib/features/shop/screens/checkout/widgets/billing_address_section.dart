8116eeb010779c439eb7442605911d95672d18d4
Uzair Ahmad <ahmaduzair355@gmail.com>
2024-09-24 18:07:32 +0500

    Phone Number updated

--- lib/features/shop/screens/checkout/widgets/billing_address_section.dart
+++ lib/features/shop/screens/checkout/widgets/billing_address_section.dart
@@ -22,7 +22,7 @@ class TBillingAddressSection extends StatelessWidget {
           children: [
             const Icon(Icons.phone, color: Colors.grey,size: 16),
             const SizedBox(width: TSizes.spaceBtwItems),
-            Text("+92 3097250069", style: Theme.of(context).textTheme.bodyMedium),
+            Text("+92 309-7250069", style: Theme.of(context).textTheme.bodyMedium),
           ],
         ),
         const SizedBox(height: TSizes.spaceBtwItems/2,),
