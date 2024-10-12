c3e9555e6ab4195c072a36ac5049c4c68e253556
Uzair Ahmad <ahmaduzair355@gmail.com>
2024-10-12 12:22:53 +0500

    terms and condition checkbox toggle

--- lib/features/authentication/screens/signup/widgets/terms_condition_checkbox.dart
+++ lib/features/authentication/screens/signup/widgets/terms_condition_checkbox.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import '../../../../../utils/constants/colors.dart';
 import '../../../../../utils/constants/sizes.dart';
 import '../../../../../utils/constants/text_strings.dart';
 import '../../../../../utils/helpers/helper_functions.dart';
+import '../../../controllers/signup/signup_controller.dart';
+
 class TTermsConditionCheckbox extends StatelessWidget {
   const TTermsConditionCheckbox({
     super.key,
@@ -10,32 +13,51 @@ class TTermsConditionCheckbox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = SignupController.instance;
     final dark = THelperFunctions.isDarkMode(context);
     return Row(
       children: [
         SizedBox(
-            height: 24,
-            width: 24,
-            child:
-            Checkbox(value: true, onChanged: (value) {})),
-        const SizedBox(width: TSizes.spaceBtwItems,),
+          height: 24,
+          width: 24,
+          child: Obx(
+            () => Checkbox(
+                value: controller.privacyPolicy.value,
+                onChanged: (value) => controller.privacyPolicy.value = !controller.privacyPolicy.value),
+          ),
+        ),
+        const SizedBox(
+          width: TSizes.spaceBtwItems,
+        ),
         Expanded(
-          child: Text.rich(TextSpan(
+          child: Text.rich(
+            TextSpan(
               children: [
-                TextSpan(text: '${TTexts.iAgreeTo} ', style: Theme.of(context).textTheme.bodySmall),
-                TextSpan(text: '${TTexts.privacyPolicy} ', style: Theme.of(context).textTheme.bodyMedium!.apply(
-                  color: dark ? TColors.white : TColors.primary,
-                  decoration: TextDecoration.underline,
-                  decorationColor: dark ? TColors.white : TColors.primary,
-                )),
-                TextSpan(text: '${TTexts.and} ', style: Theme.of(context).textTheme.bodySmall),
-                TextSpan(text: TTexts.termsOfUse, style: Theme.of(context).textTheme.bodyMedium!.apply(
-                  color: dark ? TColors.white : TColors.primary,
-                  decoration: TextDecoration.underline,
-                  decorationColor: dark ? TColors.white : TColors.primary,
-                )),
+                TextSpan(
+                    text: '${TTexts.iAgreeTo} ',
+                    style: Theme.of(context).textTheme.bodySmall),
+                TextSpan(
+                    text: '${TTexts.privacyPolicy} ',
+                    style: Theme.of(context).textTheme.bodyMedium!.apply(
+                          color: dark ? TColors.white : TColors.primary,
+                          decoration: TextDecoration.underline,
+                          decorationColor:
+                              dark ? TColors.white : TColors.primary,
+                        )),
+                TextSpan(
+                    text: '${TTexts.and} ',
+                    style: Theme.of(context).textTheme.bodySmall),
+                TextSpan(
+                    text: TTexts.termsOfUse,
+                    style: Theme.of(context).textTheme.bodyMedium!.apply(
+                          color: dark ? TColors.white : TColors.primary,
+                          decoration: TextDecoration.underline,
+                          decorationColor:
+                              dark ? TColors.white : TColors.primary,
+                        )),
               ],
-          ),),
+            ),
+          ),
         )
       ],
     );
