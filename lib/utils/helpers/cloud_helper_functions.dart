6c6888feea3d286f9bf743cd57eea38a7d29b3db
Uzair Ahmad <ahmaduzair355@gmail.com>
2024-10-14 18:16:28 +0500

    cloud helpers update

--- lib/utils/helpers/cloud_helper_functions.dart
+++ lib/utils/helpers/cloud_helper_functions.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_storage/firebase_storage.dart';
+//import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -56,34 +56,34 @@ class TCloudHelperFunctions {
   }
 
   /// Create a reference with an initial file path and name and retrieve the download URL.
-  static Future<String> getURLFromFilePathAndName(String path) async {
-    try {
-      if (path.isEmpty) return '';
-      final ref = FirebaseStorage.instance.ref().child(path);
-      final url = await ref.getDownloadURL();
-      return url;
-    } on FirebaseException catch (e) {
-      throw e.message!;
-    } on PlatformException catch (e) {
-      throw e.message!;
-    } catch (e) {
-      throw 'Something went wrong.';
-    }
-  }
+  // static Future<String> getURLFromFilePathAndName(String path) async {
+  //   try {
+  //     if (path.isEmpty) return '';
+  //     final ref = FirebaseStorage.instance.ref().child(path);
+  //     final url = await ref.getDownloadURL();
+  //     return url;
+  //   } on FirebaseException catch (e) {
+  //     throw e.message!;
+  //   } on PlatformException catch (e) {
+  //     throw e.message!;
+  //   } catch (e) {
+  //     throw 'Something went wrong.';
+  //   }
+  // }
 
   /// Retrieve the download URL from a given storage URI.
-  static Future<String> getURLFromURI(String url) async {
-    try {
-      if (url.isEmpty) return '';
-      final ref = FirebaseStorage.instance.refFromURL(url);
-      final downloadUrl = await ref.getDownloadURL();
-      return downloadUrl;
-    } on FirebaseException catch (e) {
-      throw e.message!;
-    } on PlatformException catch (e) {
-      throw e.message!;
-    } catch (e) {
-      throw 'Something went wrong.';
-    }
-  }
+  // static Future<String> getURLFromURI(String url) async {
+  //   try {
+  //     if (url.isEmpty) return '';
+  //     final ref = FirebaseStorage.instance.refFromURL(url);
+  //     final downloadUrl = await ref.getDownloadURL();
+  //     return downloadUrl;
+  //   } on FirebaseException catch (e) {
+  //     throw e.message!;
+  //   } on PlatformException catch (e) {
+  //     throw e.message!;
+  //   } catch (e) {
+  //     throw 'Something went wrong.';
+  //   }
+  // }
 }
