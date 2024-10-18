be2158b86944030bd6561a865c7a74b377ccb516
Uzair Ahmad <ahmaduzair355@gmail.com>
2024-10-18 21:31:49 +0500

    Phone number formater updated

--- lib/utils/formatters/formatter.dart
+++ lib/utils/formatters/formatter.dart
@@ -11,11 +11,11 @@ class TFormatter {
   }
 
   static String formatPhoneNumber(String phoneNumber) {
-    // Assuming a 10-digit US phone number format: (123) 456-7890
+    // Assuming a 11-digit PK phone number format: 0309 7250069
     if (phoneNumber.length == 10) {
-      return '(${phoneNumber.substring(0, 3)}) ${phoneNumber.substring(3, 6)} ${phoneNumber.substring(6)}';
+      return '(${phoneNumber.substring(0, 3)}) ${phoneNumber.substring(3)}';
     } else if (phoneNumber.length == 11) {
-      return '(${phoneNumber.substring(0, 4)}) ${phoneNumber.substring(4, 7)} ${phoneNumber.substring(7)}';
+      return '(${phoneNumber.substring(0, 4)}) ${phoneNumber.substring(4)}}';
     }
     // Add more custom phone number formatting logic for different formats if needed.
     return phoneNumber;
