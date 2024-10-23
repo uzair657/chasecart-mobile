a91d414746e06741be3fb76d9ba0c1f10082a736
Uzair Ahmad <ahmaduzair355@gmail.com>
2024-10-23 21:47:47 +0500

    General Bindings

---
+++ lib/bindings/general_bindings.dart
@@ -0,0 +1,11 @@
+import 'package:chasecart/utils/helpers/network_manager.dart';
+import 'package:get/get.dart';
+
+class GeneralBindings extends Bindings{
+  @override
+  void dependencies() {
+    // TODO: implement dependencies
+    Get.put(NetworkManager());
+  }
+
+}
